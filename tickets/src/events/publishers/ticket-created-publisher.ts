caedc7bc3cd57d0db44c0ecdd1e0658f808ed530
april <nkzhangzhang@gmail.com>
2020-07-30 13:59:52 -0700

    deep clean

--- tickets/src/events/publishers/ticket-created-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, Subjects, TicketCreatedEvent } from '@azmytickets/common';
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated;
-}
